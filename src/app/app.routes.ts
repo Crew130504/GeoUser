962b3914ac634199210ffb3d716d9ee77102f675
Crew130504 <juanpablomayo13@gmail.com>
2025-06-10 20:59:01 -0500

    Applied styles to Home: color palette, navbar, image, responsiveness

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { RegisterUserComponent } from './pages/register-user/register-user.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'register-user', component: RegisterUserComponent }
+];
+
+  
---
+++ src/app/pages/register-user/register-user.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-register-user',
+  standalone: true,
+  templateUrl: './register-user.component.html',
+  styleUrl: './register-user.component.css'
+})
+export class RegisterUserComponent {}
